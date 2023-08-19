667e6c2ae8abf7d2be5fdf0a8c340fc0d0daabe9
Zokir <example@gmail.com>
2023-08-19 17:30:10 +0100

    working now online added home.html and room.htnl

--- base/views.py
+++ base/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 
 def room(request):
-    return HttpResponse('Room page')
+   return render(request, 'room.html')
